3ef55d569460eea4870aba3b92ea314c18b539fb
YpCIIIaK <bigboyvova01@gmail.com>
2024-10-05 21:02:02 +0700

    q

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import CallToAction from './components/CallToAction';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import TrainWithUs from './components/TrainWithUs';
+import SuccessStories from './components/SuccessStories';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <TrainWithUs />
+      <SuccessStories />
+      <CallToAction />
+      <Footer />
     </div>
   );
 }
