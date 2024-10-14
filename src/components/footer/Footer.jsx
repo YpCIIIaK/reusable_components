b6db869ae3f404e4a4cdbc3825a4174f5ef08b56
YpCIIIaK <bigboyvova01@gmail.com>
2024-10-14 19:09:52 +0700

    footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = ({ links, text, styleType = 'black' }) => {
-    const baseStyles = 'flex flex-col items-center justify-center p-4 rounded-lg shadow-md';
+    const baseStyles = 'flex flex-col items-center justify-center p-4';
 
     const styleTypes = {
         black: 'bg-black text-white',
